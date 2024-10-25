735bc2b05b56ce01792090c0491e3aef0bc8c826
rahul <rahulgurjar824582@gmail.com>
2024-10-25 20:16:24 +0530

    project setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Link Shortner",
+  description: "Link shortner project create by rahul gurjar",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="favicon" href="./favicon.ico" type="image/x-icon" />
+      </head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Hello from "./hello";
+
+export default function Home() {
+  return (
+    <div>
+      {/* <Hello /> */}
+      Hello next js developer
+    </div>
+  );
+}
